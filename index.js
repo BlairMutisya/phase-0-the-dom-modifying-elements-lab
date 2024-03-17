7c9733b666f36dd208d02bde79dfa859f57dc94c
Blair <tonnyblair09@gmail.com>
2024-03-17 18:59:45 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+// Get the main element
+const main = document.getElementById('main');
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new h1 element
+newHeader.id = 'victory';
+
+// Set the text content of the new h1 element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new h1 element to the main element
+main.appendChild(newHeader);
